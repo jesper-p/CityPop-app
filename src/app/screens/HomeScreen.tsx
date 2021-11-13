c5ce5cc7c5ed89bd3634666b17d1d264fd2dc1d1
Jesper Petersson <jesperpetersson97@gmail.com>
2021-11-13 12:28:01 +0100

    HomeScreen layout done, but needs functionality

--- src/app/screens/HomeScreen.tsx
+++ src/app/screens/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import React, {FC} from "react";
-import {StyleSheet, View, Text, Button} from "react-native"
+import {StyleSheet, View, Text, Pressable} from "react-native"
 
 interface Props {
     cityButtonPressed?: string;
@@ -11,9 +11,22 @@ interface Props {
 const HomeScreen:FC<Props> = (props) => {
     return (
         <View style={styles.homeScreenContainer}>
-            <Text style={styles.cityPopText}>CityPop</Text>
-            <Button title="SEARCH BY CITY"  onPress={() => console.log(props.cityButtonPressed)} />
-            <Button title="SEARCH BY COUNTRY" onPress={() => console.log(props.countryButtonPressed)} />
+
+            <View style={styles.logoContainer}>
+                <Text style={styles.cityPopText}>CityPop</Text>
+            </View>
+            <View style={styles.buttonsContainer}>
+                <View style={styles.buttonContainer}>
+                    <Pressable style={styles.searchButton} onPress={() => console.log(props.cityButtonPressed)}>
+                        <Text style={styles.buttonText}>SEARCH BY CITY</Text>
+                    </Pressable>
+                </View>
+                    <View style={styles.buttonContainer}>
+                    <Pressable style={styles.searchButton} onPress={() => console.log(props.countryButtonPressed)}>
+                        <Text style={styles.buttonText}>SEARCH BY COUNTRY</Text>
+                    </Pressable>
+                </View>
+            </View>
         </View>
     );
 }
@@ -23,16 +36,44 @@ export default HomeScreen;
 const styles = StyleSheet.create({
     homeScreenContainer: {
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        alignItems: 'center'
     },
 
+    logoContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
 
     cityPopText: {
-        fontSize: 20,
+        fontSize: 50,
         fontWeight: 'bold',
-        margin: 16,
-        top: 30
+
+    },
+
+    buttonsContainer: {
+        flex:1,
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+    },
+
+    buttonContainer: {
+        borderWidth: 2,
+        width: 350,
+        padding: 10,
+        margin: 2
+
+    },
+
+    searchButton: {
+        justifyContent:'center',
+        alignItems: 'center',
+    },
+
+    buttonText: {
+        fontSize: 20,
+        fontWeight: 'bold'
     }
 
 });
